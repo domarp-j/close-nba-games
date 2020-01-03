248460c01c60bcdfb8b14ab6ba8b5a6d0637ea36
Pramod Jacob <pramod@prefect.io>
2020-01-03 16:03:49 -0500

    Update flow to send texts via Twilio

---
+++ close_nba_games/main.py
@@ -0,0 +1,74 @@
+from prefect import context, task, triggers, Flow, Parameter
+from prefect.client.secrets import Secret
+from twilio.rest import Client as TwilioClient
+
+# ----------------------------------------------------------------
+# Before the Run
+# ----------------------------------------------------------------
+
+# Local Runs
+#
+# Update [context.secrets] in your ~/prefect/config.toml with the
+# following values:
+#
+# PHONE_NUMBER - The phone number that will receive texts
+# TWILIO_ACCOUNT_SID
+# TWILIO_AUTH_TOKEN
+# TWILIO_PHONE_NUMBER - The phone number that will send texts
+
+# Cloud Runs
+#
+# Ensure that the secrets above are stored in your Cloud account
+
+# ----------------------------------------------------------------
+# Tasks
+# ----------------------------------------------------------------
+
+@task
+def create_twilio_client(
+  twilio_account_sid = str,
+  twilio_auth_token = str
+) -> TwilioClient:
+  return TwilioClient(
+    twilio_account_sid.get(),
+    twilio_auth_token.get()
+  )
+
+@task
+def send_reminder(
+  phone_number = str,
+  twilio_client = TwilioClient,
+  twilio_phone_number = str
+) -> None:
+  twilio_client.messages.create(
+    from_=twilio_phone_number.get(),
+    to=phone_number.get(),
+    body="Hello!"
+  )
+
+# ----------------------------------------------------------------
+# Flow
+# ----------------------------------------------------------------
+
+with Flow("Close NBA Games") as f:
+  print(repr(context.get("secrets")))
+
+  # Create Twilio client
+  twilio_client = create_twilio_client(
+    twilio_account_sid = Secret('TWILIO_ACCOUNT_SID'),
+    twilio_auth_token = Secret('TWILIO_AUTH_TOKEN')
+  )
+
+  # Send SMS
+  result = send_reminder(
+    phone_number = Secret('PHONE_NUMBER'),
+    twilio_client = twilio_client,
+    twilio_phone_number = Secret('TWILIO_PHONE_NUMBER')
+  )
+
+# ----------------------------------------------------------------
+# Local Run
+# ----------------------------------------------------------------
+
+def run():
+  f.run()
