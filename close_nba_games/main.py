930d78c70068ffce3bc0b79846be41b9cfc76ac3
Pramod Jacob <pramod@prefect.io>
2020-01-03 17:53:23 -0500

    Reach out to NBA API for close games

--- close_nba_games/main.py
+++ close_nba_games/main.py
@@ -1,5 +1,8 @@
+import requests
+
 from prefect import context, task, triggers, Flow, Parameter
 from prefect.client.secrets import Secret
+from prefect.engine import signals
 from twilio.rest import Client as TwilioClient
 
 # ----------------------------------------------------------------
@@ -11,6 +14,7 @@ from twilio.rest import Client as TwilioClient
 # Update [context.secrets] in your ~/prefect/config.toml with the
 # following values:
 #
+# NBA_API_KEY - API key for NBA stats from Rapid API
 # PHONE_NUMBER - The phone number that will receive texts
 # TWILIO_ACCOUNT_SID
 # TWILIO_AUTH_TOKEN
@@ -25,18 +29,56 @@ from twilio.rest import Client as TwilioClient
 # ----------------------------------------------------------------
 
 @task
-def create_twilio_client(twilio_account_sid, twilio_auth_token):
-  return TwilioClient(
-    twilio_account_sid.get(),
-    twilio_auth_token.get()
+def fetch_nba_games(nba_api_key):
+  r = requests.get(
+    'https://api-nba-v1.p.rapidapi.com/games/live/',
+    headers={
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+      'x-rapidapi-key': nba_api_key.get()
+    }
   )
 
+  if r.status_code != 200:
+    message = f'Received a {r.status_code} from the NBA API.'
+    context.get('logger').error(message)
+    raise signals.FAIL(message=message)
+
+  return r.json()['api']['games']
+
 @task
-def send_reminder(phone_number, twilio_client, twilio_phone_number):
+def check_for_closeness(game):
+  is_close = game['currentPeriod'] == "4/4" and abs(
+      int(game['vTeam']['score']['points']) -
+      int(game['hTeam']['score']['points'])
+    ) < 4
+
+  return { **game, 'isClose': is_close }
+
+@task
+def compose_message(games):
+  is_close = lambda game: game['isClose']
+
+  close_games = [game for game in filter(is_close, games)]
+
+  if len(close_games) == 0:
+    return None
+
+  return "There is a close NBA game on right now!"
+
+@task
+def send_message(account_sid, auth_token, message, sender, receiver):
+  if message is None:
+    return
+
+  twilio_client = TwilioClient(
+    account_sid.get(),
+    auth_token.get()
+  )
+
   twilio_client.messages.create(
-    from_=twilio_phone_number.get(),
-    to=phone_number.get(),
-    body="Hello!"
+    from_=sender.get(),
+    to=receiver.get(),
+    body=message
   )
 
 # ----------------------------------------------------------------
@@ -44,17 +86,18 @@ def send_reminder(phone_number, twilio_client, twilio_phone_number):
 # ----------------------------------------------------------------
 
 with Flow("Close NBA Games") as f:
-  # Create Twilio client
-  twilio_client = create_twilio_client(
-    twilio_account_sid = Secret('TWILIO_ACCOUNT_SID'),
-    twilio_auth_token = Secret('TWILIO_AUTH_TOKEN')
-  )
+  games = fetch_nba_games(nba_api_key = Secret('NBA_API_KEY'))
 
-  # Send SMS
-  result = send_reminder(
-    phone_number = Secret('PHONE_NUMBER'),
-    twilio_client = twilio_client,
-    twilio_phone_number = Secret('TWILIO_PHONE_NUMBER')
+  games = check_for_closeness.map(games)
+
+  message = compose_message(games)
+
+  send_message(
+    account_sid = Secret('TWILIO_ACCOUNT_SID'),
+    auth_token = Secret('TWILIO_AUTH_TOKEN'),
+    message = message,
+    receiver = Secret('PHONE_NUMBER'),
+    sender = Secret('TWILIO_PHONE_NUMBER')
   )
 
 # ----------------------------------------------------------------
